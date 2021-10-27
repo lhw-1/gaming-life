2a5e51d5df34235f7e6294b79431e5841f4ad210
lhw-1 <leehyungwoonsamuel@gmail.com>
2021-10-27 22:30:01 +0800

    Update About page text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
     const text2 = "We are a company that's fully dedicated to helping you achieve your potential. ";
     const text3 = "Founded in 2016 by John Doe, Memit has come a long way from its beginnings in a home office. "
         + "When John first started out, his passion for helping others study better drove him to research various learning methods and time management techniques. ";
-    const text4 = "Since then, he has managed to turn his research and hard work into a single handy application - Memit! "
+    const text4 = "Since then, John has managed to turn his research and hard work into a single handy application - Memit! "
         + "We now help students from all over the world, and we are positively thrilled to be a part of our students' learning experience. ";
     const text5 = "We hope that you enjoy our service as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us! ";
     return (
