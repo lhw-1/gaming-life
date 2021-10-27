b3d425ce8a9d55d2d25a08e6727b7d4658de759e
lhw-1 <leehyungwoonsamuel@gmail.com>
2021-10-27 16:48:30 +0800

    Fix server.js error for Heroku deployment

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ if (process.env.NODE_ENV === 'production') {
     });
 } else {
     app.use(express.static(path.join(__dirname, 'dist')));
-    app.set('port', process.env.PORT || 8080);
 }
 
+app.set('port', process.env.PORT || 8080);
 var server = app.listen(app.get('port'), function() {
     console.log('listening on port ', server.address().port);
 });
