6489b2b8e7ea8f9918aeb5b11c7a74db32d34b3c
lhw-1 <leehyungwoonsamuel@gmail.com>
2021-10-27 18:05:07 +0800

    Fix Heroku Deployment Issue Attempt #2

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ if (process.env.NODE_ENV === 'production') {
     // app.use(express.static('client/build'));
   
     // // Express serve up index.html file if it doesn't recognize route
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'dist'));
-    // });
-    
+
+
     app.use(express.static(path.join(__dirname, 'dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+    });
 
     console.log("LOG: Production mode...");
 
