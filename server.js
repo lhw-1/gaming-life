a1498d02943b2c1c38bfbb96c96b8c0b8b31c5c0
lhw-1 <leehyungwoonsamuel@gmail.com>
2021-10-27 16:45:01 +0800

    Update server.js for Heroku Deployment

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
     app.use(express.static('client/build'));
   
     // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-        app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
+} else {
+    app.use(express.static(path.join(__dirname, 'dist')));
+    app.set('port', process.env.PORT || 8080);
 }
-  
-var path = require('path');
-var express = require('express');
-
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+    console.log('listening on port ', server.address().port);
 });
