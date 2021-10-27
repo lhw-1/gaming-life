aae8fe628c143a83aa5aff2ce77b3b701ed54fdb
lhw-1 <leehyungwoonsamuel@gmail.com>
2021-10-27 22:29:02 +0800

    Update Warning page

--- src/components/Warning/index.js
+++ src/components/Warning/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -7,11 +7,27 @@ import * as ROUTES from '../../constants/routes';
 import '../styles.css';
 
 const Warning = () => {
+
+    const messageArr = [
+        "Is this really for you?",
+        "Did you manage to spot the games being played?",
+        "And... how many did you spot?"
+    ];
+    const [warningIndex, setWarningIndex] = useState(0);
+
     return (
         <Container fluid className="warning">
             <Row className="banner-warning">
-                <Link to={ROUTES.EXPLANATION} className="link-warning">
-                    <p className="text-warning">Is this really for you?</p>
+                <Link 
+                    to={
+                        warningIndex === 2 
+                            ? ROUTES.EXPLANATION 
+                            : ROUTES.WARNING
+                    } 
+                    onClick={() => setWarningIndex(warningIndex + 1)} 
+                    className="link-warning"
+                >
+                    <p className="text-warning">{messageArr[warningIndex]}</p>
                 </Link>
             </Row>
         </Container>
